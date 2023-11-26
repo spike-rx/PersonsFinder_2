e6d446d781a3df9ce1cd5d1af832aacd1ebb13dd
Spike <renziyang1999@outlook.com>
2023-11-26 23:43:38 +1300

    :sparkles: use redis pipeline insert data

--- src/main/kotlin/com/persons/finder/domain/services/SaveDataServiceImpl.kt
+++ src/main/kotlin/com/persons/finder/domain/services/SaveDataServiceImpl.kt
@@ -4,26 +4,40 @@ import com.persons.finder.data.Location
 import com.persons.finder.data.Person
 import com.persons.finder.repository.LocationRepository
 import com.persons.finder.repository.PersonRepository
+import org.springframework.data.geo.Point
+import org.springframework.data.redis.connection.RedisGeoCommands
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 
 /**
  * Asynchronously store big data for testing
  */
+const val size = 1000
 @Service
 class SaveDataServiceImpl(
     private val personRepository: PersonRepository,
-    private val locationsRepository: LocationRepository
+    private val locationsRepository: LocationRepository,
+    private val redisTemplate: RedisTemplate<String, Any>
 ) : SaveDataService {
     @Async
     override fun saveMillionData() {
 
         val locationList: MutableList<Location> = mutableListOf()
-        for (i in 3..1000000) {
-            val location = Location(null, i.toLong(), 9999.9999, 99999.9999)
-            locationList.add(location);
+        redisTemplate.executePipelined{connection ->
+            for (i in 3..1000000) {
+                val location = Location(null, i.toLong(), 10.123456, 20.654321)
+                connection.geoAdd(userLocationKey.toByteArray(), RedisGeoCommands.GeoLocation(i.toString().toByteArray(), Point(location.longitude!!, location.latitude)))
+                locationList.add(location)
+                if (locationList.size == 1000){
+                    locationsRepository.saveAll(locationList)
+                    locationsRepository.flush()
+                }
+
+            }
+            null
+
         }
-        locationsRepository.saveAll(locationList)
 
     }
 
